2dde244fc402a99122d85a05e21a281551deb4b5
quduss <qudshuaib@gmail.com>
2025-11-23 22:08:39 +0100

    Fix typo no-cache to no-store

--- cache.go
+++ cache.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func noCacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
 }
